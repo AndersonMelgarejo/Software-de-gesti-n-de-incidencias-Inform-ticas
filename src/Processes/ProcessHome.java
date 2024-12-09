46f5d58d1d9bc0ae9f09ff68b0fc4f558c90608c
renzito0637d <renzito0637d@gmail.com>
2024-12-08 21:22:42 -0500

    Corregido tabla en la UI_Home

--- src/Processes/ProcessHome.java
+++ src/Processes/ProcessHome.java
@@ -19,37 +19,46 @@ import java.util.Date;
  * @author franc
  */
 public class ProcessHome {
-  public static void mostrarInci(UI_Home vista, ColasIncidencias cola) {
-    String[] titulos = { "ID", "Usuario", "Departamento", "Area incidencia", "Fecha", "Descripcion", "Tipo incidencia",
-        "Prioridad" };
-    DefaultTableModel dm = new DefaultTableModel(null, titulos);
-    vista.jtHome.setModel(dm);
-    int num = 0;
-    for (Incidencias inc : cola.getCola()) {
-      num++;
-      dm.addRow(inc.Registro(num));
-    }
-  }
-
-  // mostrar por intervalo de fechas
-  public static void mostrarInciFechas(UI_Home vista, ColasIncidencias cola, Date fechaInicio, Date fechaFin) {
-    String[] titulos = { "ID", "Usuario", "Departamento", "Area incidencia", "Fecha", "Descripcion", "Tipo incidencia",
-        "Prioridad" };
-    DefaultTableModel dm = new DefaultTableModel(null, titulos);
-    vista.jtHome.setModel(dm);
-    int num = 0;
-    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-    for (Incidencias inc : cola.getCola()) {
-      try {
-        Date fechaIncidencia = formato.parse(inc.getFechaFormat());
-        if (!fechaIncidencia.before(fechaInicio) && !fechaIncidencia.after(fechaFin)) {
-          num++;
-          dm.addRow(inc.Registro(num));
+    public static void mostrarInci(UI_Home vista, ColasIncidencias cola) {
+        String[] titulos = { "ID", "Usuario Registrador", "Fecha - hora Registrada", "Departamento", 
+                             "Area Incidencia", "Descripción", "Fecha Incidencia","Tipo Incidencia",
+                             "Prioridad", "Cliente" };
+        DefaultTableModel dm = new DefaultTableModel(null, titulos);
+        vista.jtHome.setModel(dm);
+        int num = 0;
+        for (Incidencias inc : cola.getCola()) {
+            num++;
+            dm.addRow(inc.Registro(num));
         }
-      } catch (ParseException e) {
-        e.printStackTrace();
-      }
+        int anchostabla[]={15,65,100,60,60,40,50,60,60};
+        for(int i=0;i<anchostabla.length;i++){
+            vista.jtHome.getColumnModel().getColumn(i).setPreferredWidth(anchostabla[i]);
+        }
     }
-  }
 
-}
+    // mostrar por intervalo de fechas
+    public static void mostrarInciFechas(UI_Home vista, ColasIncidencias cola, Date fechaInicio, Date fechaFin) {
+        String[] titulos = { "ID", "Usuario Registrador", "Fecha - hora Registrada", "Departamento", 
+                             "Area Incidencia", "Descripción", "Fecha Incidencia","Tipo Incidencia",
+                             "Prioridad", "Cliente" };
+        DefaultTableModel dm = new DefaultTableModel(null, titulos);
+        vista.jtHome.setModel(dm);
+        int num = 0;
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        for (Incidencias inc : cola.getCola()) {
+            try {
+                Date fechaIncidencia = formato.parse(inc.getFechaFormat());
+                if (!fechaIncidencia.before(fechaInicio) && !fechaIncidencia.after(fechaFin)) {
+                    num++;
+                    dm.addRow(inc.Registro(num));
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        int anchostabla[]={15,65,100,60,60,40,50,60,60};
+            for(int i=0;i<anchostabla.length;i++){
+                vista.jtHome.getColumnModel().getColumn(i).setPreferredWidth(anchostabla[i]);
+            }
+        }
+    }
